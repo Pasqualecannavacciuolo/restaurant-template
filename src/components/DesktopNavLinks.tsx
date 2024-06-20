5005ff8d27f7217e63c1e47bca75261a48134b35
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-06-20 18:48:04 +0200

    Modifica al font

--- src/components/DesktopNavLinks.tsx
+++ src/components/DesktopNavLinks.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export default function DesktopNavLinks() {
   return (
     <div className="flex flex-row gap-5 justify-center">
-      <Link className="text-xl" to="/">
+      <Link className="text-xl prompt-light" to="/">
         Home
       </Link>
-      <Link className="text-xl" to="/menu">
+      <Link className="text-xl prompt-light" to="/menu">
         Menu
       </Link>
-      <Link className="text-xl" to="/prenota">
+      <Link className="text-xl prompt-light" to="/prenota">
         Prenota
       </Link>
       {/* Add other navigation links */}
